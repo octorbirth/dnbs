375d2e79eacfac157bbd700884b735a94257c677
bit <bit@bitcamp>
2017-11-28 14:19:30 +0900

    listPage + view(storeNumebr)

--- dnbs/src/main/java/org/bansang/mapper/StoreMapper.java
+++ dnbs/src/main/java/org/bansang/mapper/StoreMapper.java
@@ -13,7 +13,6 @@ public interface StoreMapper {
 	@Insert("insert into tbl_store (store_name, store_address, latitude, longitude, area_keyword) values (#{storeName}, #{storeAddress}, #{latitude}, #{longitude}, #{areaKeyword} )")
     public void register(RecommendDTO dto);
 
-
 	@Select("select * from tbl_store")
 	public List<RecommendDTO> listPage();
 	
@@ -24,7 +23,9 @@ public interface StoreMapper {
 	@Select("select * from tbl_store where store_name = #{storeName}  and latitude = #{latitude} and longitude = #{longitude} ")
 	public RecommendDTO exist(RecommendDTO dto);
 
-
+	@Select("select * from tbl_store where store_number = #{storeNumber}")
+    public RecommendDTO selectInfo(Long storeNumber);
+	
 }
 
 
